5e4d66ad0b47c75e07759b1957487b5af7a27639
Blacksterhack <blastertv1001@gmail.com>
2021-06-29 20:30:19 -0500

    city

---
+++ models/city.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module wich contains the city class """
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """ city class """
+    state_id = ""
+    name = ""
